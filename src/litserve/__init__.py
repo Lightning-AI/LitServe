64fa4db64ed0420e653c427ef9de7af78744fe82
Luca Antiga <luca@lightning.ai>
2024-03-07 17:57:57 -0500

    Expose root path (#10)
    * Expose root path
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/litserve/__init__.py
+++ src/litserve/__init__.py
@@ -1,8 +1,5 @@
 from litserve.__about__ import *  # noqa: F401, F403
 from litserve.api import LitAPI
-from litserve.server import LitServer
+from litserve.server import LitServer, Request, Response
 
-__all__ = [
-    "LitAPI",
-    "LitServer",
-]
+__all__ = ["LitAPI", "LitServer", "Request", "Response"]
