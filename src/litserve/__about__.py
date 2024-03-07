1baf2d08d0907bba3fec660567b5807e96b5804b
Luca Antiga <luca@lightning.ai>
2024-03-07 18:32:27 -0500

    Update release

--- src/litserve/__about__.py
+++ src/litserve/__about__.py
@@ -1,10 +1,10 @@
-__version__ = "0.0.0.dev1"
+__version__ = "0.0.0.dev2"
 __author__ = "Lightning-AI et al."
 __author_email__ = "community@lightning.ai"
 __license__ = "Apache-2.0"
-__copyright__ = f"Copyright (c) 2022-2024, {__author__}."
+__copyright__ = f"Copyright (c) 2024, {__author__}."
 __homepage__ = "https://github.com/Lightning-AI/litserve"
-__docs__ = "PyTorch Lightning Sample project."
+__docs__ = "Lightweight AI server."
 
 __all__ = [
     "__author__",
