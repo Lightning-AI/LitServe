0192e5308b5483ed3d5aa483bf237b8a01f1a148
Aniket Maurya <theaniketmaurya@gmail.com>
2024-04-10 13:59:37 -0400

    add test for litserve.server

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from litserve.server import LitServer
+import pytest
+from litserve.api import LitAPI
+from fastapi import Request, Response
+from fastapi.testclient import TestClient
+
+
+class SimpleLitAPI(LitAPI):
+    def setup(self, device):
+        self.model = lambda x: x ** 2
+
+    def decode_request(self, request: Request):
+        return request["input"]
+
+    def predict(self, x):
+        return self.model(x)
+
+    def encode_response(self, output) -> Response:
+        return {"output": output}
+
+
+@pytest.fixture()
+def lit_server():
+    return LitServer(SimpleLitAPI(), accelerator="cpu", devices=1, timeout=10)
+
+
+@pytest.fixture()
+def sync_testclient(lit_server):
+    with TestClient(lit_server.app) as client:
+        yield client
