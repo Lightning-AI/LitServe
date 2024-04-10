57bcecc173a76a098c281553240b8283886f53d6
Aniket Maurya <theaniketmaurya@gmail.com>
2024-04-10 11:40:00 -0400

    add async client

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,12 +1,14 @@
 # Copyright Lightning AI. Licensed under the Apache License 2.0, see LICENSE file.
+import asyncio
 from unittest.mock import MagicMock
+from asgi_lifespan import LifespanManager
+
 from litserve.server import run_batched_loop
 
 import pytest
 
 from fastapi import Request, Response
-from fastapi.testclient import TestClient
-from concurrent.futures import ThreadPoolExecutor
+from httpx import AsyncClient
 
 from litserve import LitAPI, LitServer
 
@@ -67,28 +69,34 @@ class SimpleLitAPI2(LitAPI):
         return {"output": float(output)}
 
 
-def test_batched():
+@pytest.mark.asyncio
+async def test_batched():
     api = SimpleLitAPI()
     server = LitServer(api, accelerator="cpu", devices=1, timeout=10, max_batch_size=2, batch_timeout=4)
 
-    with ThreadPoolExecutor(2) as executor, TestClient(server.app) as client:
-        response1 = executor.submit(client.post, "/predict", json={"input": 4.0})
-        response2 = executor.submit(client.post, "/predict", json={"input": 5.0})
+    async with LifespanManager(server.app) as manager:
+        async with AsyncClient(app=manager.app, base_url="http://test") as ac:
+            response1 = ac.post("/predict", json={"input": 4.0})
+            response2 = ac.post("/predict", json={"input": 5.0})
+            response1, response2 = await asyncio.gather(response1, response2)
 
-    assert response1.result().json() == {"output": 9.0}
-    assert response2.result().json() == {"output": 11.0}
+    assert response1.json() == {"output": 9.0}
+    assert response2.json() == {"output": 11.0}
 
 
-def test_unbatched():
+@pytest.mark.asyncio
+async def test_unbatched():
     api = SimpleLitAPI2()
     server = LitServer(api, accelerator="cpu", devices=1, timeout=10, max_batch_size=1)
 
-    with ThreadPoolExecutor(2) as executor, TestClient(server.app) as client:
-        response1 = executor.submit(client.post, "/predict", json={"input": 4.0})
-        response2 = executor.submit(client.post, "/predict", json={"input": 5.0})
+    async with LifespanManager(server.app) as manager:
+        async with AsyncClient(app=manager.app, base_url="http://test") as ac:
+            response1 = ac.post("/predict", json={"input": 4.0})
+            response2 = ac.post("/predict", json={"input": 5.0})
+            response1, response2 = await asyncio.gather(response1, response2)
 
-    assert response1.result().json() == {"output": 9.0}
-    assert response2.result().json() == {"output": 11.0}
+    assert response1.json() == {"output": 9.0}
+    assert response2.json() == {"output": 11.0}
 
 
 def test_max_batch_size():
