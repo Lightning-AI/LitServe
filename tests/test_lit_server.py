ed633aa5cca0be36bb5584fd519143ee46d33366
Aniket Maurya <theaniketmaurya@gmail.com>
2024-04-11 20:59:22 -0400

    kill process after use (#38)

--- tests/test_lit_server.py
+++ tests/test_lit_server.py
@@ -106,7 +106,7 @@ def test_single_loop(simple_litapi, loop_args):
 
 
 def test_run():
-    subprocess.Popen(
+    process = subprocess.Popen(
         ["python", "tests/simple_server.py"],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
@@ -118,3 +118,4 @@ def test_run():
     output = subprocess.run("python client.py", shell=True, capture_output=True, text=True).stdout
     assert '{"output":16.0}' in output
     os.remove("client.py")
+    process.kill()
