f6135a550fc58f47af4cea59934d00c2fa5a9e50
Aniket Maurya <theaniketmaurya@gmail.com>
2024-05-02 18:19:34 +0100

    update

--- src/litserve/specs/base.py
+++ src/litserve/specs/base.py
@@ -1,17 +1,12 @@
-from typing import Tuple, List, Callable
-
-
 class LitSpec:
+    _endpoints = []
+
     def setup(self, obj):
         raise NotImplementedError()
 
+    def _add_endpoint(self, path, endpoint, methods):
+        self._endpoints.append((path, endpoint, methods))
+
     @property
-    def endpoints(
-        self,
-    ) -> List[
-        Tuple[
-            str,
-            Callable,
-        ]
-    ]:
-        raise NotImplementedError()
+    def endpoints(self):
+        return self._endpoints.copy()
