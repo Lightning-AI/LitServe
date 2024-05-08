de53d6cc6fecf289283a1012c68eb219bdbfa2a3
Luca Antiga <luca.antiga@gmail.com>
2024-05-08 22:27:33 +0000

    Fix hanging processes from tests

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -17,7 +17,7 @@ import subprocess
 import time
 
 
-def test_e2e_default_api():
+def test_e2e_default_api(killall):
     process = subprocess.Popen(
         ["python", "tests/e2e/default_api.py"],
         stdout=subprocess.DEVNULL,
@@ -29,10 +29,10 @@ def test_e2e_default_api():
     resp = requests.post("http://127.0.0.1:8000/predict", json={"input": 4.0}, headers=None)
     assert resp.status_code == 200, f"Expected response to be 200 but got {resp.status_code}"
     assert resp.json() == {"output": 16.0}, "tests/simple_server.py didn't return expected output"
-    process.kill()
+    killall(process)
 
 
-def test_e2e_default_batching():
+def test_e2e_default_batching(killall):
     process = subprocess.Popen(
         ["python", "tests/e2e/default_batching.py"],
         stdout=subprocess.DEVNULL,
@@ -44,10 +44,10 @@ def test_e2e_default_batching():
     resp = requests.post("http://127.0.0.1:8000/predict", json={"input": 4.0}, headers=None)
     assert resp.status_code == 200, f"Expected response to be 200 but got {resp.status_code}"
     assert resp.json() == {"output": 16.0}, "tests/simple_server.py didn't return expected output"
-    process.kill()
+    killall(process)
 
 
-def test_e2e_batched_streaming():
+def test_e2e_batched_streaming(killall):
     process = subprocess.Popen(
         ["python", "tests/e2e/default_batched_streaming.py"],
         stdout=subprocess.DEVNULL,
@@ -56,7 +56,7 @@ def test_e2e_batched_streaming():
     )
 
     time.sleep(5)
-    resp = requests.post("http://127.0.0.1:8000/stream-predict", json={"input": 4.0}, headers=None, stream=True)
+    resp = requests.post("http://127.0.0.1:8000/predict", json={"input": 4.0}, headers=None, stream=True)
     assert resp.status_code == 200, f"Expected response to be 200 but got {resp.status_code}"
 
     outputs = []
@@ -66,4 +66,4 @@ def test_e2e_batched_streaming():
 
     assert len(outputs) == 10, "streaming server should have 10 outputs"
     assert {"output": 16.0} in outputs, "server didn't return expected output"
-    process.kill()
+    killall(process)
