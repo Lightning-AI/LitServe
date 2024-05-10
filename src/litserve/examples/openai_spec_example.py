a3d1eb5182a0bac115748058ec9f1884fd2e0499
Aniket Maurya <theaniketmaurya@gmail.com>
2024-05-10 14:38:04 +0100

    fix windows tests

--- src/litserve/examples/openai_spec_example.py
+++ src/litserve/examples/openai_spec_example.py
@@ -1,5 +1,34 @@
+# Copyright The Lightning AI team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import litserve as ls
 from litserve.specs.openai import OpenAISpec
+
+
+class OpenAIBasicAPI(ls.LitAPI):
+    def setup(self, device):
+        self.model = ...
+
+    def predict(self, x):
+        return "This is a generated output"
+
+
+class OpenAISpecWithHooks(OpenAISpec):
+    def decode_request(self, request):
+        return request
+
+    def encode_response(self, output):
+        return {"text": output}
 
 
 class OpenAILitAPI(ls.LitAPI):
@@ -18,5 +47,5 @@ class OpenAILitAPI(ls.LitAPI):
 
 if __name__ == "__main__":
     spec = OpenAISpec()
-    server = ls.LitServer(OpenAILitAPI(), spec=spec)
+    server = ls.LitServer(OpenAIBasicAPI(), spec=spec)
     server.run(port=8000)
--- src/litserve/specs/openai.py
+++ src/litserve/specs/openai.py
@@ -1,3 +1,16 @@
+# Copyright The Lightning AI team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import time
 import typing
 from typing import Literal, Optional, List, Dict, Union
@@ -7,6 +20,8 @@ from .base import LitSpec
 from pydantic import BaseModel, Field
 
 from ..utils import wait_for_queue_timeout, LitAPIStatus
+import sys
+import asyncio
 
 if typing.TYPE_CHECKING:
     pass
@@ -100,9 +115,17 @@ class OpenAISpec(LitSpec):
 
         responses = []
         for uid, read in zip(uids, pipes):
-            data = await wait_for_queue_timeout(
-                self._server.data_reader(read), self._server.app.timeout, uid, self._server.app.request_buffer
-            )
+            if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith("win"):
+                data = await wait_for_queue_timeout(
+                    asyncio.to_thread(self._server.get_from_pipe, read),
+                    self._server.app.timeout,
+                    uid,
+                    self._server.app.request_buffer,
+                )
+            else:
+                data = await wait_for_queue_timeout(
+                    self._server.data_reader(read), self._server.app.timeout, uid, self._server.app.request_buffer
+                )
             responses.append(data)
 
         choices = []
