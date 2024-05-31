5050b7ffc9f7fe59e96fd591e45e0348d6adb721
Aniket Maurya <theaniketmaurya@gmail.com>
2024-05-31 17:14:15 +0100

    fix

--- tests/test_lit_server.py
+++ tests/test_lit_server.py
@@ -386,8 +386,6 @@ async def test_inject_context(mocked_load_and_raise):
 
     api = PredictError()
     server = LitServer(api)
-    with pytest.raises(
-        TypeError, match=re.escape("PredictError.predict() missing 1 required positional argument: 'y'")
-    ):
+    with pytest.raises(TypeError, match=re.escape("predict() missing 1 required positional argument: 'y'")):
         async with LifespanManager(server.app) as manager, AsyncClient(app=manager.app, base_url="http://test") as ac:
             resp = await ac.post("/predict", json={"input": 5.0}, timeout=10)
