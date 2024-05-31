27bd74586203606764e7b2d0fbc6addde3d7a365
bhimrazy <bhimrajyadav977@gmail.com>
2024-05-31 14:33:59 +0545

    feat: Add tool_calls support to TestAPIWithToolCalls in default_openaispec_tools.py

--- tests/e2e/default_openaispec_tools.py
+++ tests/e2e/default_openaispec_tools.py
@@ -1,6 +1,23 @@
-from litserve import OpenAISpec
-from litserve.examples.openai_spec_example import TestAPIWithToolCalls
 import litserve as ls
+from litserve import OpenAISpec
+from litserve.examples.openai_spec_example import TestAPI
+from litserve.specs.openai import ChatMessage
+
+
+class TestAPIWithToolCalls(TestAPI):
+    def encode_response(self, output):
+        yield ChatMessage(
+            role="assistant",
+            content="",
+            tool_calls=[
+                {
+                    "id": "call_abc123",
+                    "type": "function",
+                    "function": {"name": "get_current_weather", "arguments": '{\n"location": "Boston, MA"\n}'},
+                }
+            ],
+        )
+
 
 if __name__ == "__main__":
     server = ls.LitServer(TestAPIWithToolCalls(), spec=OpenAISpec())
