39986bfdd09369d1d6830e4c9970e1c5e4e0fb00
bhimrazy <bhimrajyadav977@gmail.com>
2024-08-23 12:50:00 +0545

    chore: Update sleep duration

--- src/litserve/server.py
+++ src/litserve/server.py
@@ -665,7 +665,7 @@ class LitServer:
             response, status = None, None
             try:
                 while not task.done():
-                    asyncio.sleep(0.01)
+                    asyncio.sleep(0.2)
                     if hasattr(request, "is_disconnected") and await request.is_disconnected():
                         task.cancel()
                         break
