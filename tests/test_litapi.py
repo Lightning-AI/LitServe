82ad04fd072b6bd5be0282f424a398e10f8c9ce7
Aniket Maurya <theaniketmaurya@gmail.com>
2024-08-25 22:15:55 +0100

    add tests

--- tests/test_litapi.py
+++ tests/test_litapi.py
@@ -48,8 +48,8 @@ def test_default_batch_unbatch():
     api._sanitize(max_batch_size=4, spec=None)
     inputs = [1, 2, 3, 4]
     output = api.batch(inputs)
-    assert output == inputs
-    assert api.unbatch(output) == inputs
+    assert output == inputs, "Default batch should not change input"
+    assert api.unbatch(output) == inputs, "Default unbatch should not change input"
 
 
 def test_custom_batch_unbatch():
@@ -57,8 +57,8 @@ def test_custom_batch_unbatch():
     api._sanitize(max_batch_size=4, spec=None)
     inputs = [1, 2, 3, 4]
     output = api.batch(inputs)
-    assert np.all(output == np.array(inputs))
-    assert api.unbatch(output) == inputs
+    assert np.all(output == np.array(inputs)), "Custom batch stacks input as numpy array"
+    assert api.unbatch(output) == inputs, "Custom unbatch should unstack input as list"
 
 
 def test_batch_unbatch_stream():
@@ -66,6 +66,16 @@ def test_batch_unbatch_stream():
     api._sanitize(max_batch_size=4, spec=None)
     inputs = [1, 2, 3, 4]
     output = api.batch(inputs)
-    assert np.all(output == np.array(inputs))
+    output = api.predict(output)
+    output = api.unbatch(output)
+    output = api.encode_response(output)
+    first_resp = [o["output"] for o in next(output)]
+    expected_outputs = [[0, 0, 0, 0], [1, 2, 3, 4], [2, 4, 6, 8], [3, 6, 9, 12]]
+    assert first_resp == expected_outputs[0], "First response should be 0s"
+    count = 1
+    for out, expected_output in zip(output, expected_outputs[1:]):
+        resp = [o["output"] for o in out]
+        assert resp == expected_output
+        count += 1
 
-    pass
+    assert count == 4, "Should have 4 responses"
