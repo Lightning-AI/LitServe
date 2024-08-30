b1aaee41a8e79c2db0a13084d6cd246b6038e5db
Aniket Maurya <theaniketmaurya@gmail.com>
2024-08-30 16:27:01 +0100

    add Starlette middleware support (#253)
    * add tests
    
    * test update tests
    
    * update type

--- tests/test_lit_server.py
+++ tests/test_lit_server.py
@@ -23,6 +23,8 @@ import torch
 import torch.nn as nn
 from httpx import AsyncClient
 from litserve.utils import wrap_litserve_start
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from unittest.mock import patch, MagicMock
 import pytest
@@ -393,3 +395,37 @@ def test_custom_middleware():
         assert response.status_code == 200, f"Expected response to be 200 but got {response.status_code}"
         assert response.json() == {"output": 16.0}, "server didn't return expected output"
         assert response.headers["X-Request-Id"] == "00000"
+
+
+def test_starlette_middlewares():
+    middlewares = [
+        (
+            TrustedHostMiddleware,
+            {
+                "allowed_hosts": ["localhost", "127.0.0.1"],
+            },
+        ),
+        HTTPSRedirectMiddleware,
+    ]
+    server = ls.LitServer(ls.examples.SimpleLitAPI(), middlewares=middlewares)
+    with wrap_litserve_start(server) as server, TestClient(server.app) as client:
+        response = client.post("/predict", json={"input": 4.0}, headers={"Host": "localhost"})
+        assert response.status_code == 200, f"Expected response to be 200 but got {response.status_code}"
+        assert response.json() == {"output": 16.0}, "server didn't return expected output"
+
+        response = client.post("/predict", json={"input": 4.0}, headers={"Host": "not-trusted-host"})
+        assert response.status_code == 400, f"Expected response to be 400 but got {response.status_code}"
+
+
+def test_middlewares_inputs():
+    server = ls.LitServer(SimpleLitAPI(), middlewares=[])
+    assert len(server.middlewares) == 1, "Default middleware should be present"
+
+    server = ls.LitServer(ls.examples.SimpleLitAPI(), middlewares=[], max_payload_size=1000)
+    assert len(server.middlewares) == 2, "Default middleware should be present"
+
+    server = ls.LitServer(ls.examples.SimpleLitAPI(), middlewares=None)
+    assert len(server.middlewares) == 1, "Default middleware should be present"
+
+    with pytest.raises(ValueError, match="middlewares must be a list of tuples"):
+        ls.LitServer(ls.examples.SimpleLitAPI(), middlewares=(RequestIdMiddleware, {"length": 5}))
