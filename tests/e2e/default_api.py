525b2075fe016e622472c7de1be63349c7f80d1c
Aniket Maurya <theaniketmaurya@gmail.com>
2024-08-30 20:12:18 +0100

    fix tests

--- tests/e2e/default_api.py
+++ tests/e2e/default_api.py
@@ -14,6 +14,6 @@
 import litserve as ls
 
 if __name__ == "__main__":
-    api = ls.examples.SimpleLitAPI()
+    api = ls.test_examples.SimpleLitAPI()
     server = ls.LitServer(api)
     server.run(port=8000)
