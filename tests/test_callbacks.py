a48e0c36af6bd763fe6f0ff506eb27a1b635732c
Aniket Maurya <theaniketmaurya@gmail.com>
2024-09-17 01:29:51 +0530

    update

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -1,16 +1,6 @@
 import litserve as ls
 from fastapi.testclient import TestClient
-import logging
-import tempfile
-
-
 from litserve.utils import wrap_litserve_start
-
-# Create a temporary file for logging
-log_file = tempfile.NamedTemporaryFile(delete=False)
-log_file.close()
-# Ensure logging is configured to output to the temporary file
-logging.basicConfig(filename=log_file.name, level=logging.INFO)
 
 
 def test_callback():
