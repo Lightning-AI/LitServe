0697222de00632c25cdfaa34c3a8784aec073f45
bhimrazy <bhimrajyadav977@gmail.com>
2024-09-17 12:12:58 +0545

    refactor classnames

--- tests/multiple_litserver.py
+++ tests/multiple_litserver.py
@@ -15,29 +15,29 @@ from litserve.server import LitServer, run_all
 from litserve.test_examples import SimpleLitAPI
 
 
-class SimpleLitAPI1(SimpleLitAPI):
+class MultipleLitServerAPI1(SimpleLitAPI):
     def setup(self, device):
         self.model = lambda x: x**1
 
 
-class SimpleLitAPI2(SimpleLitAPI):
+class MultipleLitServerAPI2(SimpleLitAPI):
     def setup(self, device):
         self.model = lambda x: x**2
 
 
-class SimpleLitAPI3(SimpleLitAPI):
+class MultipleLitServerAPI3(SimpleLitAPI):
     def setup(self, device):
         self.model = lambda x: x**3
 
 
-class SimpleLitAPI4(SimpleLitAPI):
+class MultipleLitServerAPI4(SimpleLitAPI):
     def setup(self, device):
         self.model = lambda x: x**4
 
 
 if __name__ == "__main__":
-    server1 = LitServer(SimpleLitAPI1(), api_path="/predict-1")
-    server2 = LitServer(SimpleLitAPI2(), api_path="/predict-2")
-    server3 = LitServer(SimpleLitAPI3(), api_path="/predict-3")
-    server4 = LitServer(SimpleLitAPI4(), api_path="/predict-4")
-    run_all([server1, server2, server3, server4], port=8000)
+    server1 = LitServer(MultipleLitServerAPI1(), api_path="/predict-1")
+    server2 = LitServer(MultipleLitServerAPI2(), api_path="/predict-2")
+    server3 = LitServer(MultipleLitServerAPI3(), api_path="/predict-3")
+    server4 = LitServer(MultipleLitServerAPI4(), api_path="/predict-4")
+    run_all([server1, server2, server3, server4], port=8000, num_api_servers=2)
