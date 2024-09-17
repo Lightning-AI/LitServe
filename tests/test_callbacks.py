7c9b51861085c4d0f120669fc59d19f69437171b
Aniket Maurya <theaniketmaurya@gmail.com>
2024-09-17 12:57:50 +0530

    add test

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -1,8 +1,10 @@
 import re
+from unittest.mock import MagicMock
 
 import litserve as ls
 from fastapi.testclient import TestClient
 
+from litserve.callbacks import CallbackRunner, EventTypes
 from litserve.callbacks.defaults import MetricLogger
 from litserve.utils import wrap_litserve_start
 
@@ -18,3 +20,18 @@ def test_callback(capfd):
     captured = capfd.readouterr()
     pattern = r"Prediction took \d+\.\d{2} seconds"
     assert re.search(pattern, captured.out), f"Expected pattern not found in output: {captured.out}"
+
+
+def test_metric_logger():
+    cb = MetricLogger()
+    cb_runner = CallbackRunner()
+    cb_runner.add_callbacks(cb)
+    assert cb_runner._callbacks == [cb], "Callback not added to runner"
+    cb_runner.trigger_event(EventTypes.LITAPI_PREDICT_START)
+    cb_runner.trigger_event(EventTypes.LITAPI_PREDICT_END)
+    cb.on_litapi_predict_start = MagicMock()
+    cb.on_litapi_predict_end = MagicMock()
+    cb_runner.trigger_event(EventTypes.LITAPI_PREDICT_START)
+    cb_runner.trigger_event(EventTypes.LITAPI_PREDICT_END)
+    cb.on_litapi_predict_start.assert_called_once(), "on_litapi_predict_start not called"
+    cb.on_litapi_predict_end.assert_called_once(), "on_litapi_predict_end not called"
