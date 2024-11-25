a913fcbf9c9719eb77425eb447e34e5594422bbc
bhimrazy <bhimrajyadav977@gmail.com>
2024-11-25 13:44:59 +0545

    updated example

--- src/litserve/test_examples/openai_embedding_spec_example.py
+++ src/litserve/test_examples/openai_embedding_spec_example.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 
 from litserve.api import LitAPI
@@ -7,11 +9,11 @@ class TestAPI(LitAPI):
     def setup(self, device):
         self.model = None
 
-    def decode_request(self, request):
+    def decode_request(self, request)->List[str]:
         return request.input if isinstance(request.input, list) else [request.input]
 
-    def predict(self, x):
+    def predict(self, x)-> List[List[float]]:
         return np.random.rand(len(x), 768).tolist()
 
-    def encode_response(self, response):
-        return {"embeddings": response}
+    def encode_response(self, output)-> dict:
+        return {"embeddings": output}
