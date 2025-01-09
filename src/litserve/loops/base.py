747308adde767d0e96cf10a252d2d8f419a9f5aa
Aniket Maurya <theaniketmaurya@gmail.com>
2025-01-09 14:51:22 +0000

    add validation for `stream=False` with `yield` usage (#402)
    * add validation for stream=False and yield usage
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update base.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update streaming_loops.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/litserve/loops/base.py
+++ src/litserve/loops/base.py
@@ -264,6 +264,28 @@ class DefaultLoop(LitLoop):
             return
 
         original = lit_api.unbatch.__code__ is LitAPI.unbatch.__code__
+        if not lit_api.stream and any([
+            inspect.isgeneratorfunction(lit_api.predict),
+            inspect.isgeneratorfunction(lit_api.encode_response),
+        ]):
+            raise ValueError(
+                """When `stream=False`, `lit_api.predict`, `lit_api.encode_response` must not be
+                generator functions.
+
+                Correct usage:
+
+                    def predict(self, inputs):
+                        ...
+                        return {"output": output}
+
+                Incorrect usage:
+
+                    def predict(self, inputs):
+                        ...
+                        for i in range(max_token_length):
+                            yield prediction
+                """
+            )
         if (
             lit_api.stream
             and lit_api.max_batch_size > 1
