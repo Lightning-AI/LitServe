d8677eb7e125bf8c718b8877aae1a21fc70c8a51
Aniket Maurya <theaniketmaurya@gmail.com>
2025-07-02 14:10:21 +0530

    fix

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -31,7 +31,7 @@ def e2e_from_file(filename):
         @wraps(test_fn)
         def wrapper(*args, **kwargs):
             process = subprocess.Popen(
-                ["uv run", filename],
+                ["python", filename],
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
                 stdin=subprocess.DEVNULL,
